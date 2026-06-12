c0b77039493b4c6ae5994873182bcd31670b25de
hima_sree <himasree2507@gmail.com>
2026-06-12 13:20:05 +0530

    Update todoRoutes.js

--- Backend/routes/todoRoutes.js
+++ Backend/routes/todoRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get("/getUser/:todoId",todoController.getTodo)
 router.post("/login", todoController.authTodo);
 router.post("/register", todoController.createTodo);
-router.post("/todo/:id", todoController.deleteTodo);
+router.delete("/todo/:id", todoController.deleteTodo);
 
 export default router
