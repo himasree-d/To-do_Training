803227ff154e4e00c2eec34d134305d7aebf0fdf
hima_sree <himasree2507@gmail.com>
2026-06-12 14:55:33 +0530

    Update todoRoutes.js

--- Backend/routes/todoRoutes.js
+++ Backend/routes/todoRoutes.js
@@ -1,10 +1,10 @@
-import express from "express"
-import * as todoController from "../controller/todoController.js"
-const router = express.Router();
+import * as todoController from "../controller/todoController.js";
 
-router.get("/getUser/:todoId",todoController.getTodo)
+router.get("/getUser/:id", todoController.getUser);
 router.post("/login", todoController.authTodo);
 router.post("/register", todoController.createTodo);
+router.get("/todos/:id", todoController.getTodos);
+router.post("/todos/:id", todoController.addTodo);
 router.delete("/todo/:id", todoController.deleteTodo);
 
-export default router
+export { getUser, authTodo, createTodo, getTodos, addTodo, deleteTodo };
